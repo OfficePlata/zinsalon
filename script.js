08b940abd9dd6d5d84d0b83d36467a24501a07c5
OfficePlata <150496622+OfficePlata@users.noreply.github.com>
2025-10-18 06:58:14 +0900

    Update GAS_API_URL to new endpoint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  * 初期設定
  * =================================================================
  */
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyRby3RJGEZouMt46t4aXfg9KYS_CrLrtdDo4QXLTXS-CpFoXcoJ6S4tUVQ2B71Uvyb0g/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyzbzejbdUJ5qGP-QEYCjYyTihxFjnWemeqsOZvlzAMgCqSF2Wg2ZSuecX0feIxqr_6dQ/exec';
 // ご指定のLarkフォーム共有URL
 const LARK_FORM_URL = 'https://bjplm1vnnisz.jp.larksuite.com/share/base/form/shrjpQCxuI9Sa1Q0QC4Y2xgn8Fb'; 
 let userProfile = null;
